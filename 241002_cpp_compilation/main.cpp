286056c236e979ae3dd196bf76ee310873be1199
NoRiceOnigiri <nomorethensand@gmail.com>
2024-10-02 22:45:39 +0300

    Failed to create cpp_compilation app

---
+++ 241002_cpp_compilation/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int x = 5;
+	cout << "Введите приветсвие: "
+	string str;
+	getline(cin, str);
+	cout << str << endl;
+}
+	
