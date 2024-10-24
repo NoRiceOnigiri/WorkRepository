7d7be10fdc33d2cd5bfe7eef0077fc098c95b780
NoRiceOnigiri <nomorethensand@gmail.com>
2024-10-24 13:51:27 +0300

    Code re-refactoring

--- after_refactoring_code.cpp
+++ after_refactoring_code.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 
 float arithmetic_mean(const int* const arr, const int size);
@@ -8,29 +7,6 @@ void print_array(const int* const arr, const int size);
 
 int main()
 {
-    // int* arr1 = new int[10];
-    // fill_array(arr1);
-
-    // std::cout << "Первая последовательность: ";
-    // print_array(arr1);
-
-    // std::cout 
-    //     << "Среднее первое последовательности: " 
-    //     << arithmetic_mean(arr1) 
-    //     << std::endl;
-
-    // int* arr2 = new int[10];
-    // fill_array(arr2);
-    // std::cout << "Вторая последовательность: ";
-    // print_array(arr2);
-    // std::cout << "Среднее второй последовательности: " << arithmetic_mean(arr2) << std::endl;
-
-    // int* arr3 = new int[10];
-    // fill_array(arr3);
-    // std::cout << "Третья последовательность: ";
-    // print_array(arr3);
-    // std::cout << "Среднее третьей последовательности: " << arithmetic_mean(arr3) << std::endl;
-
     std::cout << "Введите количество массивов: ";
     int arr_count;
     std::cin >> arr_count;
@@ -83,8 +59,9 @@ void create_array(const int arr_index)
     fill_array(arr, arr_size);
 
     std::cout
-        << "Последовательность #: "
-        << arr_index;
+        << "Последовательность #"
+        << arr_index
+        << ": ";
     print_array(arr, arr_size);
 
     std::cout 
